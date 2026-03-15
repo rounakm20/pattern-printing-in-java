784207789f194de77188e2a9311e30ed43a11f8e
Rounak Mishra <rounakm5886@gmail.com>
2026-03-16 00:22:12 +0530

    Floyd's Triangle implementation

--- loydsTrianglePattern.java
+++ loydsTrianglePattern.java
@@ -13,16 +13,16 @@
 
 
 
-public class App {
+public class FloydsTrianglePattern {
     public static void main(String[] args) {
         int n = 5;
         int nums = 1;
-        for(int i = 1; i<=n; i++){
-            for(int j = 1; j <=i ; j++){
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(nums + " ");
                 nums++;
             }
-             
             System.out.println();
         }
     }
