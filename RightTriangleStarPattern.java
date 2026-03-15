972e2bf3d9642227bb6c13809f1a958fd8762358
Rounak Mishra <rounakm5886@gmail.com>
2026-03-16 00:28:40 +0530

    add right triangle star pattern program in java

--- RightTriangleStarPattern.java
+++ RightTriangleStarPattern.java
@@ -10,13 +10,13 @@
 // *****
 
 
-public class App {
+public class RightTriangleStarPattern {
     public static void main(String[] args) {
         int n = 5;
-        for(int i =1; i<=n ; i++){
-            for(int j = 1; j<=i ; j++){
-                System.out.print("*");
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
